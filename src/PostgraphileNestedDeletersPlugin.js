75ef4e35b9bed7796d48aa2a1ee947f39379576e
Francesco Venica <francesco.venica@gmail.com>
2025-08-20 11:36:42 +0200

    update deps

--- src/PostgraphileNestedDeletersPlugin.js
+++ src/PostgraphileNestedDeletersPlugin.js
@@ -68,9 +68,8 @@ module.exports = function PostGraphileNestedDeletersPlugin(builder) {
         if (isNodeIdDeleter) {
           const nodeId = input[nodeIdFieldName];
           const primaryKeys = foreignTable.primaryKeyConstraint.keyAttributes;
-          const { Type, identifiers } = build.getTypeAndIdentifiersFromNodeId(
-            nodeId,
-          );
+          const { Type, identifiers } =
+            build.getTypeAndIdentifiersFromNodeId(nodeId);
           if (Type !== ForeignTableType) {
             throw new Error('Mismatched type');
           }
